e317baf32e17f826121215eedb79a34e9be6a146
simon-tanna <gcab022109@coderacademy.edu.au>
2022-08-10 09:56:47 +1000

    Count.sol contract complete

--- Count.sol
+++ Count.sol
@@ -1,12 +1,17 @@
 pragma solidity ^0.8.0;
+// SPDX-License-Identifier: MIT
 
 contract Counter {
     // contract code
     // creating a count variable
-    uint count; // this is a state variable. uint wil be on a positive integer
+    uint public count = 0; 
+    // this is a state variable. uint wil be on a positive integer
+    // the public keyword allows the count variable to be displayed for free
 
-    function getCount() public { // public refers to visibility of the function. it means that it can be called outside the contract
-        return count;
+    function incrementCount() public {
+        // assigns variable and saves it back to the original variable
+        // this is a write function as it adds it to the blockchain. it will cost gas+
+        count++;
     }
 
 }
