6a79b595fa181338ada8f6523740eec9629582b2
simon-tanna <gcab022109@coderacademy.edu.au>
2022-08-10 14:37:04 +1000

    Completed conditionals and loops

---
+++ MyContractConditionalsLoops.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract MyContractConditionalsLoops {
+    uint256[] public numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function countEvenNumbers() public view returns (uint256) {
+        uint256 count = 0;
+
+        for (uint256 i = 0; i < numbers.length; i++) {
+            if (isEvenNumber(numbers[i])) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    // Conditionals
+
+    function isEvenNumber(uint256 _number) public view returns (bool) {
+        if (_number % 2 == 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function isOwner() public view returns (bool) {
+        return (msg.sender == owner);
+    }
+}
